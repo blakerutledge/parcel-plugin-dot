4403516b505eccd4153835c4dbe09e1e9e8eefa5
blakerutledge <blake@blakerutledge.com>
2021-11-24 12:50:22 -0500

    update parcel 2 dep

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 module.exports = function(bundler) {
   
   bundler.addAssetType( "dot", require.resolve("./DotAsset") );
-
-  console.log('blake fork')
-
-  // bundler.on('buildStart',)
-
-
+  
 };
